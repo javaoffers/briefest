aec2d3a0e138957dbc8f6ed1777a7e3ce1b1e840
caomingjie <caomingjiecode@outlook.com>
2021-12-13 10:36:50 +0800

    inti

--- model-helper/src/main/java/com/javaoffers/batis/modelhelper/convert/Date2LongConvert.java
+++ model-helper/src/main/java/com/javaoffers/batis/modelhelper/convert/Date2LongConvert.java
@@ -1,10 +1,10 @@
-//package com.javaoffers.batis.modelhelper.convert;
-//
-//import java.util.Date;
-//
-//public class Date2LongConvert extends AbstractConver<Date,Long> {
-//    @Override
-//    public Long convert(Date date) {
-//        return date.getTime();
-//    }
-//}
+package com.javaoffers.batis.modelhelper.convert;
+
+import java.util.Date;
+
+public class Date2LongConvert extends AbstractConver<Date,Long> {
+    @Override
+    public Long convert(Date date) {
+        return date.getTime();
+    }
+}

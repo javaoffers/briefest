0bf36578f3b8b943e7195b43d8f0220e5cc00aeb
caomingjie <caomingjiecode@outlook.com>
2022-07-10 02:53:01 +0800

    Mapper 功能完善 delete

---
+++ model-sample/src/main/java/com/javaoffers/base/modelhelper/sample/spring/SpringSuportCrudUserMapperDelete.java
@@ -0,0 +1,46 @@
+package com.javaoffers.base.modelhelper.sample.spring;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javaoffers.base.modelhelper.sample.spring.mapper.CrudUserMapper;
+import com.javaoffers.base.modelhelper.sample.spring.model.User;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+@SpringBootApplication
+@RequestMapping
+@MapperScan("com.javaoffers.base.modelhelper.sample.spring.mapper")
+public class SpringSuportCrudUserMapperDelete implements InitializingBean {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Resource
+    CrudUserMapper crudUserMapper;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSuportCrudUserMapperDelete.class, args);
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        crudUserMapper.delete()
+                .where()
+                .eq(User::getId, 2)
+                .ex();
+
+        System.exit(0);
+
+    }
+
+    public void print(Object user) throws JsonProcessingException {
+        System.out.println(objectMapper.writeValueAsString(user));
+    }
+}

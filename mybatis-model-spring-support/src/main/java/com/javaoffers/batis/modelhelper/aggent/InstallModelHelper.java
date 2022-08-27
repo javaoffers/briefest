d6777f5ac7b94e866e287fb7b67523645f3ee716
caomingjie <caomingjiecode@outlook.com>
2022-08-27 17:40:47 +0800

    Optimized architecture and documentation

--- mybatis-model-spring-support/src/main/java/com/javaoffers/batis/modelhelper/aggent/InstallModelHelper.java
+++ mybatis-model-spring-support/src/main/java/com/javaoffers/batis/modelhelper/aggent/InstallModelHelper.java
@@ -1,5 +1,6 @@
 package com.javaoffers.batis.modelhelper.aggent;
 
+import com.javaoffers.batis.modelhelper.exception.VersionException;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
@@ -8,46 +9,56 @@ import org.apache.ibatis.builder.annotation.MapperAnnotationBuilder;
 import org.apache.ibatis.builder.annotation.MapperAnnotationBuilderAggent;
 import org.apache.ibatis.type.TypeAliasRegistry;
 
-import java.lang.reflect.Method;
-
 /**
  * install. Currently only supported My Batis3.4.1
  */
 public class InstallModelHelper {
 
+    private static final String MYBATIS_VERSION = "3.4.1";
+
     public static void install(){
 
         ByteBuddyAgent.install();
 
-        new ByteBuddy()
-                .redefine(MapperMethodAggent.class)
-                .name(MapperMethod.class.getName())
-                .make()
-                .load(MapperMethod.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+        install341();
+    }
 
-        new ByteBuddy()
-                .redefine(MapperProxyAggent.class)
-                .name(MapperProxy.class.getName())
-                .make()
-                .load(MapperProxy.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+    private static void install341() {
+        try {
+            new ByteBuddy()
+                    .redefine(MapperMethodAggent.class)
+                    .name(MapperMethod.class.getName())
+                    .make()
+                    .load(MapperMethod.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
 
-        new ByteBuddy()
-                .redefine(TypeAliasRegistryAggent.class)
-                .name(TypeAliasRegistry.class.getName())
-                .make()
-                .load(TypeAliasRegistry.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+            new ByteBuddy()
+                    .redefine(MapperProxyAggent.class)
+                    .name(MapperProxy.class.getName())
+                    .make()
+                    .load(MapperProxy.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
 
-        new ByteBuddy()
-                .redefine(MapperAnnotationBuilderAggent.class)
-                .name(MapperAnnotationBuilder.class.getName())
-                .make()
-                .load(MapperAnnotationBuilder.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+            new ByteBuddy()
+                    .redefine(TypeAliasRegistryAggent.class)
+                    .name(TypeAliasRegistry.class.getName())
+                    .make()
+                    .load(TypeAliasRegistry.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
 
-        new ByteBuddy()
-                .redefine(MapperProxyFactoryAggent.class)
-                .name(MapperProxyFactory.class.getName())
-                .make()
-                .load(MapperProxyFactory.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+            new ByteBuddy()
+                    .redefine(MapperAnnotationBuilderAggent.class)
+                    .name(MapperAnnotationBuilder.class.getName())
+                    .make()
+                    .load(MapperAnnotationBuilder.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+
+            new ByteBuddy()
+                    .redefine(MapperProxyFactoryAggent.class)
+                    .name(MapperProxyFactory.class.getName())
+                    .make()
+                    .load(MapperProxyFactory.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+        }catch (Exception e){
+            throw new VersionException("This version is compatible with mybatis "+MYBATIS_VERSION+"," +
+                    " please make sure that the version of mybatis is "+MYBATIS_VERSION);
+        }
+
     }
 
 }
---
+++ mybatis-model-spring-support/src/main/java/com/javaoffers/batis/modelhelper/exception/VersionException.java
@@ -0,0 +1,11 @@
+package com.javaoffers.batis.modelhelper.exception;
+
+/**
+ * Version compatibility exception.
+ * create by cmj
+ */
+public class VersionException extends RuntimeException {
+    public VersionException(String message) {
+        super(message);
+    }
+}

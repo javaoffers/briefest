802e2f912350d2e2885558bdbabb74aed9f31b0d
caomingjiecode <caomingjiecode@outlook.com>
2023-03-13 20:05:52 +0800

    Solve the same table join decryption

--- mybatis-model-encipher/src/main/java/com/javaoffers/batis/modelhelper/encrypt/parser/NamingFromItemVisitorAdapter.java
+++ mybatis-model-encipher/src/main/java/com/javaoffers/batis/modelhelper/encrypt/parser/NamingFromItemVisitorAdapter.java
@@ -6,6 +6,8 @@ import com.javaoffers.thrid.jsqlparser.statement.select.FromItemVisitorAdapter;
 import com.javaoffers.thrid.jsqlparser.statement.select.SelectBody;
 import com.javaoffers.thrid.jsqlparser.statement.select.SubSelect;
 
+import java.util.HashMap;
+
 /**
  * @author mingJie
  */
@@ -24,26 +26,31 @@ public class NamingFromItemVisitorAdapter extends FromItemVisitorAdapter {
         if(alias != null){
             namingContent.putAliasNameAndRealTableName(alias.getName(), table.getName());
         }else {
-            //单表查询.并且无别名.()
+            //Single table query. And no alias.()
             namingContent.putAliasNameAndRealTableName(table.getName(), table.getName() );
         }
 
-        //如果是子查询，则记录子查询的主表. (先解析from table, 后解析join table)
+        //If it is a subquery, record the main table of the subquery. (First parse the from table, then parse the join table)
         if(namingContent.isSubSelct() && namingContent.getSubSelectMainTable() == null){
             namingContent.setSubSelectMainTable(table.getName());
         }
 
     }
 
+    /**
+     * join (select xx from table where xx ) alias on xxx
+     * @param subSelect
+     */
     public void visit(SubSelect subSelect) {
 
         SelectBody selectBody = subSelect.getSelectBody();
         NamingSelectContent namingContent = new NamingSelectContent();
         namingContent.setSubSelct(true);
         namingContent.setProcessor(this.namingContent.getProcessor());
-        //这里直接使用 namingContent.中的mapper。 不用新建因为from xx join (select xx) aliasName
-        //在最后的where时可能会用到这里的join 子表别名
-        namingContent.setTableNameMapper(this.namingContent.getTableNameMapper());
+        //Here you need to create a new TableNameMapper. Because from xx join (select xx) aliasName. The subquery has been completely decrypted.
+        //Accessing the query field of the subquery in on does not need to be decrypted, because it has been decrypted in the subquery
+        //That is, the alias of the subquery used in where to access the data does not need to be decrypted. Because it has been decrypted in the subquery
+        namingContent.setTableNameMapper(new HashMap<>(this.namingContent.getTableNameMapper()));
         selectBody.accept(new NamingSelectVisitorAdapter(namingContent));
 
     }

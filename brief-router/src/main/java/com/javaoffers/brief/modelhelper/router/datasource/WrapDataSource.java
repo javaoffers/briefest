c2af99bc8a619a6ec720492ea02e4963357f98b2
caomingjiecode <caomingjiecode@outlook.com>
2023-10-17 09:57:45 +0800

    动态路由

---
+++ brief-router/src/main/java/com/javaoffers/brief/modelhelper/router/datasource/WrapDataSource.java
@@ -0,0 +1,62 @@
+package com.javaoffers.brief.modelhelper.router.datasource;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+/**
+ * 对客户使用的数据源进行包装一下.
+ * @author mingJie
+ */
+public class WrapDataSource implements DataSource {
+
+    private DataSource dataSource;
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return this.dataSource.getConnection();
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return this.dataSource.getConnection(username, password);
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return this.dataSource.unwrap(iface);
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return this.dataSource.isWrapperFor(iface);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return this.dataSource.getLogWriter();
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        this.dataSource.setLogWriter(out);
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        this.dataSource.setLoginTimeout(seconds);
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return this.dataSource.getLoginTimeout();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return this.dataSource.getParentLogger();
+    }
+}

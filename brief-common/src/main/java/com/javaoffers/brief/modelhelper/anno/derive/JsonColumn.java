fd68428f18359a71e9c830789d6a83efd77f4774
javaofferss <caomingjiecode@outlook.com>
2023-11-05 12:41:54 +0800

    json column

--- brief-common/src/main/java/com/javaoffers/brief/modelhelper/anno/derive/JsonColumn.java
+++ brief-common/src/main/java/com/javaoffers/brief/modelhelper/anno/derive/JsonColumn.java
@@ -1,9 +1,11 @@
 package com.javaoffers.brief.modelhelper.anno.derive;
 
+import java.io.Serializable;
+
 /**
  * 标记是一个json字段. 如果你的表中有json字段, 那么你只需要建一个类继承JsonColumn即可.
  * 注意: 综合考虑没有使用注解的方式去做.
  * @author mingJie
  */
-public interface JsonColumn {
+public interface JsonColumn extends Serializable {
 }

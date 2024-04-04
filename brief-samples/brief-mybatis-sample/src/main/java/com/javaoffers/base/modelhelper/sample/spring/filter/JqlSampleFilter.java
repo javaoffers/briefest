18008c0b6fcc8ed862e28d0ced9dda557deb4ffb
javaofferss <caomingjiecode@outlook.com>
2024-04-04 11:56:24 +0800

    brief context init

--- brief-samples/brief-mybatis-sample/src/main/java/com/javaoffers/base/modelhelper/sample/spring/filter/JqlSampleFilter.java
+++ brief-samples/brief-mybatis-sample/src/main/java/com/javaoffers/base/modelhelper/sample/spring/filter/JqlSampleFilter.java
@@ -1,16 +1,16 @@
 package com.javaoffers.base.modelhelper.sample.spring.filter;
 
 import com.javaoffers.brief.modelhelper.filter.JqlExecutorChain;
-import com.javaoffers.brief.modelhelper.filter.JqlChainFilter;
+import com.javaoffers.brief.modelhelper.filter.JqlExecutorFilter;
 import com.javaoffers.brief.modelhelper.filter.JqlMetaInfo;
 
 /**
  * @description: 测试filter
  * @author: create by cmj on 2023/6/1 21:01
  */
-public class JqlSampleFilter implements JqlChainFilter {
+public class JqlSampleFilter implements JqlExecutorFilter {
     @Override
-    public Object filter(JqlExecutorChain<Object, JqlMetaInfo> chain) {
+    public Object filter(JqlExecutorChain chain) {
         System.out.println("-------------------------- 0 0 ---------------------- ");
         return chain.doChain();
 

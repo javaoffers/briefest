b95df69cc5e9303bf42c360a4daa9b2ec02c8916
caomingjiecode <caomingjiecode@outlook.com>
2024-04-18 15:33:46 +0800

    add delete sample

--- brief-samples/brief-mybatis-sample/src/main/java/com/javaoffers/base/modelhelper/sample/spring/SpringSuportCrudUserMapperDelete.java
+++ brief-samples/brief-mybatis-sample/src/main/java/com/javaoffers/base/modelhelper/sample/spring/SpringSuportCrudUserMapperDelete.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javaoffers.base.modelhelper.sample.mapper.BriefUserMapper;
 import com.javaoffers.base.modelhelper.sample.model.User;
+import com.javaoffers.brief.modelhelper.core.Id;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RequestMapping
@@ -30,7 +34,7 @@ public class SpringSuportCrudUserMapperDelete implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         testDelete();
-
+        testBatchDelete();
         if(status){
             System.exit(0);
         }
@@ -44,6 +48,29 @@ public class SpringSuportCrudUserMapperDelete implements InitializingBean {
                 //.isNotNull(User::getId)
                 .isNull(User::getId)
                 .ex();
+    }
+
+    public void testBatchDelete(){
+        List<User> ls = new ArrayList<User>();
+        for(int i=0; i< 103; i++){
+            User user = User.builder().name("h1_delete_"+i).build();
+            ls.add(user);
+        }
+        List<Id> ids = crudUserMapper.general().saveBatch(ls);
+        System.out.println("---------------> insert size "+ids.size());
+
+        List<User> userList = crudUserMapper.select().colAll().where().like(User::getName, "h1_delete_").exs();
+
+        System.out.println("-----------> select size " + userList.size());
+
+        int i = crudUserMapper.general().removeByIds(userList.stream().map(User::getId).collect(Collectors.toList()));
+
+        System.out.println("-------------> delete size " + i);
+
+        List<User> userList2 = crudUserMapper.select().colAll().where().like(User::getName, "h1_delete_").exs();
+
+        System.out.println("-----------> select size " + userList2.size());
+
     }
 
     public void print(Object user) throws JsonProcessingException {

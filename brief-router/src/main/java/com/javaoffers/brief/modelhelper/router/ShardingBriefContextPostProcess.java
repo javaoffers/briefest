42c8b53291f851f9f8655041b81c41c0cebea41c
caomingjie <caomingjie@xiaomi.com>
2025-04-11 16:50:49 +0800

    sharding coding

--- brief-router/src/main/java/com/javaoffers/brief/modelhelper/router/ShardingBriefContextPostProcess.java
+++ brief-router/src/main/java/com/javaoffers/brief/modelhelper/router/ShardingBriefContextPostProcess.java
@@ -1,17 +1,14 @@
 package com.javaoffers.brief.modelhelper.router;
 
 import com.javaoffers.brief.modelhelper.context.BriefContext;
-import com.javaoffers.brief.modelhelper.context.BriefContextAware;
 import com.javaoffers.brief.modelhelper.context.BriefContextPostProcess;
 import com.javaoffers.brief.modelhelper.jdbc.JdbcExecutorFactory;
 import com.javaoffers.brief.modelhelper.router.jdbc.ShardingBriefJdbcExecutorFactory;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 /**
  * 该类主要用于修改BriefContext组件. 在finish之前.
  */
-public class ShardingBriefContextAware implements BriefContextPostProcess {
+public class ShardingBriefContextPostProcess implements BriefContextPostProcess {
 
     @Override
     public void postProcess(BriefContext briefContext) {

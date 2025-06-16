321eeef7f042d46c8008a2041d6beacd6fd728b1
caomingjie <caomingjie@xiaomi.com>
2025-06-16 10:17:42 +0800

    fix #46 bug convertor stack over

--- brief-helper/src/main/java/com/javaoffers/brief/modelhelper/core/ConvertDelegate.java
+++ brief-helper/src/main/java/com/javaoffers/brief/modelhelper/core/ConvertDelegate.java
@@ -19,8 +19,6 @@ public class ConvertDelegate<T> implements ConvertProxy<T>{
 
     static ConvertRegisterSelectorDelegate choseConverter = ConvertRegisterSelectorDelegate.convert;
 
-    private AtomicInteger defaultConvertSuccessCount = new AtomicInteger();
-
     /**
      * 如果是基础类会被升级
      */
@@ -46,18 +44,17 @@ public class ConvertDelegate<T> implements ConvertProxy<T>{
         try {
             ConvertRegisterSelectorDelegate.processingConvertClass.set(orgDes);
             desObject = (T) convert.convert(srcUpgrade.cast(srcValue));
-            defaultConvertSuccessCount.incrementAndGet();
         }catch (Exception e){
-            defaultConvertSuccessCount.decrementAndGet();
             //重新选择转换
             ConvertDelegate<?> convertDelegate = choseConverter.choseConverter(orgDes, srcValue);
             //选择是否替换convert. 如果失败次数小于５则进行替换
-            if(defaultConvertSuccessCount.get() < -5){
-                this.convert = convertDelegate.convert;
+            if(this.convert != convertDelegate.convert){
+               this.convert = convertDelegate.convert;
                 this.srcUpgrade = convertDelegate.srcUpgrade;
-                defaultConvertSuccessCount.set(0);
+                desObject = (T) convertDelegate.convert(srcValue);
+            }else {
+                throw e;
             }
-            desObject = (T) convertDelegate.convert(srcValue);
         }finally {
             ConvertRegisterSelectorDelegate.processingConvertClass.remove();
         }

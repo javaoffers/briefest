919012426386da8f62609cf67be415893eac1e6f
javaofferss <caomingjiecode@outlook.com>
2025-10-12 21:25:42 +0800

    op

--- brief-samples/brief-speedier-sample/src/main/java/com/javaoffers/base/modelhelper/sample/sharding/ShardingSample.java
+++ brief-samples/brief-speedier-sample/src/main/java/com/javaoffers/base/modelhelper/sample/sharding/ShardingSample.java
@@ -72,7 +72,7 @@ public class ShardingSample {
     public void testShardingSampleGt(){
         List<ShardingUser> exs = userBriefMapper.select().colAll().where()
                 .gtEq(ShardingUser::getBirthday,  DateUtils.addDays(new Date(), -31))
-//                .ltEq(ShardingUser::getBirthday,  new Date()) //重复分片会自动报错
+                .ltEq(ShardingUser::getBirthday,  new Date()) //重复分片会自动报错
                 .exs();
         System.out.println(exs.size());
     }

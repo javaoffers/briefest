9490b11a14bdeb7dcee3ed095d0705403a5c5196
caomingjie <caomingjie@xiaomi.com>
2026-02-09 20:48:15 +0800

    add fun map() and maps()

---
+++ brief-core/src/main/java/com/javaoffers/brief/modelhelper/fun/MapMoreFun.java
@@ -0,0 +1,12 @@
+package com.javaoffers.brief.modelhelper.fun;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MapMoreFun {
+    /**
+     * Query data natively
+     * @return map
+     */
+    List<Map<String, Object>> maps();
+}
---
+++ brief-core/src/main/java/com/javaoffers/brief/modelhelper/fun/MapOneFun.java
@@ -0,0 +1,11 @@
+package com.javaoffers.brief.modelhelper.fun;
+
+import java.util.Map;
+
+public interface MapOneFun {
+    /**
+     * Query data natively
+     * @return map
+     */
+    Map<String, Object> map();
+}
